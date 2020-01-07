55e55f42be477d9cc1d950bbfc11c362d9d445b6
gvanel <gvanel@insa-lyon.fr>
2020-01-08 04:44:07 +0900

    corrected date formatting error

--- assets/js/size-evolution-chart.js
+++ assets/js/size-evolution-chart.js
@@ -19,10 +19,10 @@ function drawSizeEvolutionChart(apps, apps_to_draw) {
 	
 	// Create scales
 	let xScale = d3.scaleLinear().range([50, width - 50]).domain([
-		d3.min(Object.values(apps), app => d3.min(app["versions"], a => moment(a[0], "DD-MM-YYYY").unix())),
-		d3.max(Object.values(apps), app => d3.max(app["versions"], a => moment(a[0], "DD-MM-YYYY").unix()))]);
+		d3.min(Object.values(apps), app => d3.min(app["versions"], a => moment(a[0], "YYYY-MM-DD").unix())),
+		d3.max(Object.values(apps), app => d3.max(app["versions"], a => moment(a[0], "YYYY-MM-DD").unix()))]);
 	let yScale = d3.scaleLinear().range([10, height - 100]).domain([d3.max(Object.values(apps), app => d3.max(app["versions"], a => a[1])), 0]);
-	
+
 	let defs = svg.append("defs");
 	
 	// Define the div for the tooltip
@@ -36,7 +36,7 @@ function drawSizeEvolutionChart(apps, apps_to_draw) {
 	
 	svg.append("g")
 		.call(d3.axisBottom(xScale)
-			.tickFormat(d => moment(new Date(d)).format("DD/MM/YYYY")))
+			.tickFormat(d => moment(new Date(d*1000)).format("DD/MM/YYYY")))
 		.attr("transform", `translate(0, ${height - 100})`)
 		.selectAll("text")
 		.attr("transform", "translate(20, 20) rotate(45)");
@@ -47,14 +47,14 @@ function drawSizeEvolutionChart(apps, apps_to_draw) {
 		let versions = apps[appName]["versions"];
 		// Sort versions
 		versions.sort(function (a, b) {
-			return moment(a[0], "DD-MM-YYYY").unix() - moment(b[0], "DD-MM-YYYY").unix();
+			return moment(a[0], "YYYY-MM-DD").unix() - moment(b[0], "YYYY-MM-DD").unix();
 		});
 
 		var path = svg.append("path")
 			.datum(versions)
 			.attr("d", d3.line()
 					.x(function (d) {
-						return xScale(moment(d[0], "DD-MM-YYYY").unix())
+						return xScale(moment(d[0], "YYYY-MM-DD").unix())
 					})
 					.y(function (d) {
 						return yScale(d[1])
